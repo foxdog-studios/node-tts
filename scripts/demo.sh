dacc9afac8c88e37ba1f9a9948217e4352e2d81d
Lloyd Henning <lloyd.w.henning@gmail.com>
2013-08-18 15:02:27 +0100

    Added a basic demo script

---
+++ scripts/demo.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+usage()
+{
+    echo "
+
+
+    Usage:
+
+        # $(basename "${0}") [-]
+"
+    exit 1
+}
+
+while getopts : opt; do
+    case "${opt}" in
+        \?|*) usage ;;
+    esac
+done
+unset opt
+
+shift $(( OPTIND - 1 ))
+
+if [[ "${#}" != 0 ]]; then
+    usage
+fi
+unset usage
+
+REPO=$(realpath "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/..")
+cd -- "${REPO}"
+
+# =============================================================================
+# = Clean-up                                                                  =
+# =============================================================================
+
+clean_up() {
+    killall -9 chromium node python || true
+}
+
+trap clean_up SIGINT SIGTERM EXIT
+
+
+# =============================================================================
+# = Helpers                                                                   =
+# =============================================================================
+
+term() {
+    xterm -e "${@}" & disown
+}
+
+term ./scripts/run.sh
+
+term grunt
+
+chromium --incognito http://localhost:8000
+
